a9d5c9f8aab7d6b20f2653374327a106956158bd
Teresa de Castro <decastro.ter@gmail.com>
2022-07-06 11:09:39 -0400

    testAddGetDVD

--- src/test/java/com/sg/dvdlibrary/dao/DVDLibraryDaoFileImplTest.java
+++ src/test/java/com/sg/dvdlibrary/dao/DVDLibraryDaoFileImplTest.java
@@ -5,6 +5,7 @@
  */
 package com.sg.dvdlibrary.dao;
 
+import com.sg.dvdlibrary.dto.DVD;
 import java.io.FileWriter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
@@ -21,6 +22,9 @@ public class DVDLibraryDaoFileImplTest {
     
     DVDLibraryDao testDao;
     
+    public DVDLibraryDaoFileImplTest() {
+    }
+    
     @BeforeEach
     public void setUp() throws Exception {
         String testFile = "testlibrary.txt";
@@ -29,5 +33,50 @@ public class DVDLibraryDaoFileImplTest {
         testDao = new DVDLibraryDaoFileImpl(testFile);
     }
 
+    @Test
+    public void testAddGetDVD() throws Exception {
+       // Create our method test inputs
+       String dvdID = "0001";
+       DVD dvd = new DVD(dvdID);
+       dvd.setTitle("Edward Scissorhands");
+       dvd.setReleaseDate("December 7, 1990");
+       dvd.setMpaaRating("PG-13");
+       dvd.setDirectorsName("Tim Burton");
+       dvd.setStudioName("20th Century Fox");
+       dvd.setUserRating("One of a kind!");
+       
+       // Add the DVD to the DAO
+       testDao.addDVD(dvdID, dvd);
+       // Get the DVD from the DAO
+       DVD retrievedDVD = testDao.getDVD(dvdID);
+       
+       // Check the data is equal
+       assertEquals(dvd.getDVDID(), 
+               retrievedDVD.getDVDID(),
+               "Checking DVD ID.");
+       
+       assertEquals(dvd.getTitle(), 
+               retrievedDVD.getTitle(),
+               "Checking DVD Title.");
+       
+       assertEquals(dvd.getReleaseDate(), 
+               retrievedDVD.getReleaseDate(), 
+               "Checking DVD Release Date.");
+       
+       assertEquals(dvd.getMpaaRating(), 
+               retrievedDVD.getMpaaRating(), 
+               "Checking DVD MPAA Rating.");
+       
+       assertEquals(dvd.getDirectorsName(), 
+               retrievedDVD.getDirectorsName(), 
+               "Checking DVD Directors's Name.");
+       
+       assertEquals(dvd.getStudioName(), 
+               retrievedDVD.getStudioName(), 
+               "Checking DVD Studio Name");
+       
+       assertEquals(dvd.getUserRating(), 
+               retrievedDVD.getUserRating(), 
+               "Checking DVD comments.");    }
     
 }
