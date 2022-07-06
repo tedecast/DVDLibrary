5fed42f2c96abe1ef5c7c338094ff0048bc181a8
Teresa de Castro <decastro.ter@gmail.com>
2022-07-06 11:30:21 -0400

    Added removedDVD to test, prior to serviceLayer

--- src/test/java/com/sg/dvdlibrary/dao/DVDLibraryDaoFileImplTest.java
+++ src/test/java/com/sg/dvdlibrary/dao/DVDLibraryDaoFileImplTest.java
@@ -120,4 +120,64 @@ public class DVDLibraryDaoFileImplTest {
                 "The list of DVDs should include Beetlejuice.");
     }
     
+    @Test
+    public void testRemoveDVD() throws Exception {
+        // Create two new DVDs
+        DVD dvd1 = new DVD("0001");
+        dvd1.setTitle("Edward Scissorhands");
+        dvd1.setReleaseDate("December 7, 1990");
+        dvd1.setMpaaRating("PG-13");
+        dvd1.setDirectorsName("Tim Burton");
+        dvd1.setStudioName("20th Century Fox");
+        dvd1.setUserRating("One of a kind!");
+        
+        DVD dvd2 = new DVD ("0002");
+        dvd2.setTitle("Beetlejuice");
+        dvd2.setReleaseDate("March 30, 1988");
+        dvd2.setMpaaRating("PG");
+        dvd2.setDirectorsName("Tim Burton");
+        dvd2.setStudioName("Warner Bros.");
+        dvd2.setUserRating("Spooky OOky!");
+        
+        // Add both to the DAO
+        testDao.addDVD(dvd1.getDVDID(), dvd1);
+        testDao.addDVD(dvd2.getDVDID(), dvd2);
+        
+        // remove the first DVD - Edward Scissorhands
+        DVD removedDVD = testDao.removeDVD(dvd1.getDVDID());
+        
+        // Check that the correct object was removed.
+        assertEquals(removedDVD, dvd1, "The removed DVD should be Edward Scissorhands.");
+        
+        // Get all DVDs
+        List<DVD> allDVDs = testDao.getAllDVDs();
+        
+        // First check the general contents of the list
+        assertNotNull(allDVDs, "All DVDs should be not null.");
+        assertEquals(1, allDVDs.size(), "All DVDs should only have 1 DVD.");
+        
+        // Then the specifics
+        assertFalse(allDVDs.contains(dvd1), "All DVDs should NOT include Edward Scissorhands.");
+        assertTrue(allDVDs.contains(dvd2), "All DVDs should NOT include Beetlejuice.");
+        
+        // Remove the second student
+        removedDVD = testDao.removeDVD(dvd2.getDVDID());
+        // Check that the correct object was removed.
+        assertEquals(removedDVD, dvd2, "The removed DVD should be Beetlejuice.");
+        
+        // retrieve all of the students again, and check the list.
+        allDVDs = testDao.getAllDVDs();
+        
+        // Check the contents of the list - it should be empty
+        assertTrue(allDVDs.isEmpty(), "The retrieved list of DVDs should be empty.");
+        
+        // Try to 'get' both DVDs by their old id - they should be null!
+        DVD retrievedDVD = testDao.getDVD(dvd1.getDVDID());
+        assertNull(retrievedDVD, "Edward Scissorhands was removed, should be null.");
+        
+        retrievedDVD = testDao.getDVD(dvd2.getDVDID());
+        assertNull(retrievedDVD, "Beetlejuice was removed, should be null.");
+                
+    }
+    
 }
